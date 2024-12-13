fed35640c8142550971ac5d5de21dc287b596180
MoulikGupta08 <moulikgupta08@gmail.com>
2024-12-13 12:20:34 +0530

    Stacks

---
+++ Stacks.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int value;
+        Node* next;
+        
+        Node(int value){
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class Stack 
+{
+    private:
+        int height;
+        Node* top;
+    public:
+        Stack(int value){
+            Node* newNode = new Node(value);
+                top = newNode;
+                height = 1;
+        }
+
+        void push(int value){
+            Node* newNode = new Node(value);
+            newNode->next = top;
+            top = newNode;
+            height++;
+        }
+
+        int pop(){
+            if(height == 0){
+                return INT8_MIN;
+            }
+
+            Node* temp = top;
+            int poppedValue = top->value;
+            top = top->next;
+            delete temp;
+            height--;
+            return poppedValue;
+        }
+
+        void printDetails(){
+            cout<<"Top : "<<top->value<<endl;
+            cout<<"Height : "<<height<<endl;
+
+            Node* temp = top;
+            while(top != nullptr){
+                top = top->next;
+                cout<<temp->value<<endl;
+                temp = top;
+            }
+        }
+};
+
+int main()
+{
+    Stack* myStack = new Stack(0);
+
+    myStack->push(1);
+    myStack->push(2);
+    myStack->push(3);
+    myStack->push(4);
+    
+
+    myStack->pop();
+    myStack->pop();
+    myStack->pop();
+
+    myStack->printDetails();
+
+    return 0;
+}
